eccd4e942c84f01f5421a1e3bef3c387d43c18ff
pascallaub <nutritionexpertition@gmail.com>
2025-06-18 18:24:17 +0200

    push

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,17 @@
-variable "aws_region" {}
-variable "tf_state_bucket" {}
-variable "ssh_public_key" {}
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "tf_state_bucket" {
+  description = "S3 bucket for Terraform state"
+  type        = string
+  default     = "react-cicd-terraform-state-1750250738"  
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
